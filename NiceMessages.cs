e05ef169007f33ed7084ce1595fb83dbb0d57e83
JFFCarleton <julienfriedfavreau@cmail.carleton.ca>
2020-05-21 20:58:02 -0400

    Add project files.

---
+++ NiceMessages.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewValley;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using System.IO;
+using System.Collections;
+
+namespace Nice_Messages
+{
+    class NiceMessages {
+        private string[] loadInMessages;
+        private string[] newDayMessages;
+        private IModHelper helperMcHelperson; //because this is a hobby project and I think he's a cute helper!
+
+     
+        public NiceMessages(string season){
+                this.newDayMessages = File.ReadAllLines(
+                    helperMcHelperson.Content.Load<string>(getNewSeasonMessages(season), ContentSource.ModFolder)
+                );
+        }
+       
+        private string getNewSeasonMessages(string season){
+            switch (season){
+                case "spring":
+                    return "springMessages.txt";
+
+                case "summer":
+                    return "summerMessages.txt";
+
+                case "fall":
+                    return "fallMessages.txt";
+
+                case "winter":
+                    return "winterMessages.txt";
+            }
+                //Monitor.Log("no season found", LogLevel.Error);
+            return null;
+        }
+
+        public string randomMorningMessage() {
+            //todo, add in RNG. set to 0 for now for testing
+            return this.newDayMessages[0];
+        }
+        public String[] getLoadInMessages() { return this.loadInMessages; }
+    }
+}
---
+++ NiceMessagesMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace Nice_Messages
+{
+    public class NiceMessagesMain : Mod
+    {
+        private NiceMessages niceMessages;
+        private IModHelper oneHelpyBoi;
+        private String currSeason;
+        
+        //SM Api set up
+        public override void Entry(IModHelper oneHelpyBoi){
+            oneHelpyBoi.Events.GameLoop.SaveLoaded += GameLoop_SaveLoaded;
+            oneHelpyBoi.Events.GameLoop.DayStarted += GameLoop_DayStarted;
+            oneHelpyBoi.Events.GameLoop.DayEnding += GameLoop_DayEnding;
+        }
+
+        //listeners
+        private void GameLoop_SaveLoaded(object sender, SaveLoadedEventArgs e){
+            this.currSeason = Game1.currentSeason;
+            this.niceMessages = new NiceMessages(currSeason);
+            Game1.showGlobalMessage("Game Save Loaded!");
+        }
+
+        private void GameLoop_DayStarted(object sender, DayStartedEventArgs e){
+            changeSeason(currSeason);
+            Game1.showGlobalMessage("Hi i'm a test! "+currSeason+" "+niceMessages.randomMorningMessage());
+        }
+        
+        private void GameLoop_DayEnding(object sender, DayEndingEventArgs e){
+            return;
+        }
+        
+        //methods
+        private void changeSeason(String checkSeason) {
+            if (this.currSeason == Game1.currentSeason) { return; }
+            this.currSeason = Game1.currentSeason;
+            this.niceMessages = new NiceMessages(currSeason);
+        }
+    
+    }
+}
